e6a7c273279688a979e31bd7d323bd94bf9652fb
Vladislav Archebasov <archebasovvl@gmail.com>
2025-11-04 18:08:17 +0300

    Implement baseline

--- scripts/utils.r
+++ scripts/utils.r
@@ -32,3 +32,54 @@ create_id <- function(series) {
 
     ids
 }
+
+fit_baseline <- function(user_rating_matrix) {
+    items_means <- vector("numeric", ncol(user_rating_matrix))
+    for (j in seq_len(ncol(user_rating_matrix))) {
+        count <- 0
+        sum <- .0
+        for (i in seq_len(nrow(user_rating_matrix))) {
+            if (user_rating_matrix[i, j] != 0) {
+                count <- count + 1
+                sum <- sum + user_rating_matrix[i, j]
+            }
+        }
+        items_means[j] <- sum / count
+    }
+
+    for (i in seq_len(nrow(user_rating_matrix))) {
+        for (j in seq_len(ncol(user_rating_matrix))) {
+            if (user_rating_matrix[i, j] == 0) {
+                user_rating_matrix[i, j] <- items_means[j]
+            }
+        }
+    }
+
+    user_rating_matrix
+}
+
+predict_baseline <- function(model, user_rating_matrix, user, user_map) {
+    mask <- user_rating_matrix == 0
+    user_id <- user_map[[user]]
+
+    model[mask] <- 0
+    print(dim(model))
+    predictions <- model[user_id, ]
+
+    predictions
+}
+
+choose_k_best <- function(row, k) {
+    index <- numeric(k)
+
+    row <- matrix(append(row, seq_along(row)), nrow = 2, ncol = length(row), TRUE)
+    row <- row[, order(row[1, ], decreasing = TRUE)]
+
+    for (i in seq_len(k)) {
+        index[i] <- row[2, i]
+    }
+
+    index
+}
+
+print(choose_k_best(c(1.1, 4.4, 2.2, 0.7), 2))
